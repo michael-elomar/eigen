3747a936d3953e19432bd89e2bad41ee342c6539
Michael El Omar <michaelelomar@outlook.com>
2026-04-09 20:29:33 +0200

    export includes in atom.mk

--- atom.mk
+++ atom.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ###############################################################################
-#  swig
+#  eigen
 ###############################################################################
 include $(CLEAR_VARS)
 LOCAL_MODULE := eigen
@@ -11,6 +11,6 @@ LOCAL_ARCHIVE_VERSION := 3.4.1
 LOCAL_ARCHIVE := $(LOCAL_MODULE)-$(LOCAL_ARCHIVE_VERSION).tar.gz
 LOCAL_ARCHIVE_SUBDIR := $(LOCAL_MODULE)-$(LOCAL_ARCHIVE_VERSION)
 
-# LOCAL_EXPORT_LDLIBS := -llz4
+LOCAL_EXPORT_C_INCLUDES := $(TARGET_OUT_STAGING)/usr/include/eigen3
 
 include $(BUILD_CMAKE)
